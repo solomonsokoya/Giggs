2f91266dd818b9b57be3f0f6677d90809a87951b
Solomon sokoya <solomonsokoya718@gmail.com>
2018-07-20 15:30:37 -0400

    Added employer register component

--- react-views/src/App.js
+++ react-views/src/App.js
@@ -3,12 +3,10 @@ import {connect} from 'react-redux';
 import {user_worker, user_employer} from './reducers';
 import Home from './Components/home';
 import WorkerRegister from './Components/workerRegister';
+import EmployerRegister from './Components/employerRegister';
 
 const mapStateToProps = state => {
-  const {
-      worker,
-      employer
-  } = state;
+  const {worker, employer} = state;
   return {worker, employer}
 }
 const mapDispatchToProps = dispatch => ({
@@ -17,32 +15,21 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class App extends Component {
-
-
-
-
   render() {
+    const {handleUserWorker, handleUserEmployer} = this.props
+    let View;
 
-        // fetch('/employers/1').then( data => data.json()).then( respBody => console.log(respBody));
-        console.log(this.props.worker)
-        const{
-          handleUserWorker,
-          handleUserEmployer
-        } = this.props
-        let View;
-
-        if (!this.props.worker && !this.props.employer){
-          View = <Home handleUserWorker = {handleUserWorker} handleUserEmployer ={handleUserEmployer} />
-        }
-        else if(this.props.worker){
-          View = <WorkerRegister/>
-        }
-    return (
-      <div>
-        {View}
-      </div>
-    );
+    if (!this.props.worker && !this.props.employer) {
+      View = <Home handleUserWorker={handleUserWorker} handleUserEmployer={handleUserEmployer}/>
+    } else if (this.props.worker) {
+      View = <WorkerRegister/>
+    }
+    else if(this.props.employer){
+      View = <EmployerRegister/>
+    }
+    return (<div>
+      {View}
+    </div>);
   }
 }
-
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ react-views/src/Components/employerRegister.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+class employerRegister extends Component {
+  render() {
+    return (
+      <div className = "loginGrid">
+        <div className = "headline">Join Giggs Today</div>
+        <form className = "form" >
+          <input type="text" name="name" placeholder="Name"/>
+          <input type="text" name="logo" placeholder="logo url"/>
+          <input type="email" name="email" placeholder="email"/>
+          <input type="password" name="password" placeholder="password"/>
+          <input type="submit" value = "register"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default employerRegister;
