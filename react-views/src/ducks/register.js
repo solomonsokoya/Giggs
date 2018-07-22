0b896c796896b82b4db9b11fe0b512fc615febcb
Solomon sokoya <solomonsokoya718@gmail.com>
2018-07-22 17:58:03 -0400

    Made new action type, action creator, and reducer for

--- react-views/src/ducks/register.js
+++ react-views/src/ducks/register.js
@@ -1,9 +1,18 @@
 const WORKER = 'WORKER';
 const EMPLOYER = 'EMPLOYER';
+cost LOGIN_PAGE_CHANGE = 'LOGIN_PAGE_CHANGE';
 
 export const user_worker = res => ({type: WORKER, payload: res})
 
 export const user_employer = res => ({type: EMPLOYER, payload: res})
+
+export const loginPageChange = (key, value) => ({
+  type: LOGIN_PAGE_CHANGE,
+  payload: {
+    key,
+    value
+  }
+});
 
 const defaultState = {
   name: '',
@@ -34,6 +43,8 @@ export const reducer = (state = defaultState, action) => {
         email,
         password
       });
+      case LOGIN_PAGE_CHANGE:
+      return Object.assign({}, state, {[action.payload.key]: action.payload.value})
     default:
       return state
   }
